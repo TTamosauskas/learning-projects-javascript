542b486939d0779a9f4e972892fe47b649797d41
TTamosauskas <thiago.gt@gmail.com>
2020-02-27 11:28:26 -0300

    feat: funcionamento via mouse

--- keyboard-drummer/drummer.js
+++ keyboard-drummer/drummer.js
@@ -1,25 +1,46 @@
 //Cria função de remoção de transição
-playSound = (e) => {
+tocaTecla = (e) => {
     // Pega div com data-key igual a keycode de e
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-
+0
     // Pega  tag audio com data-key igual keycode de e
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-    // se audio não existir encerra iteração
-    if (!audio) return;
+    //Chama função sonora
+    TocaSom(key, audio);
 
-    //Adiciona classe playing a div
-    key.classList.add('playing');
-
-    //Volta o play para zero.. caso já tenha tocado pode tocar de novo
-    audio.currentTime = 0;
-
-    //Toca audio
-    audio.play();
-
-    
   }
+
+tocaBotao = (e) => {
+
+    //Pega div com numero do div
+    const key = document.querySelector(`div[data-key="${e}"]`);
+
+    //Pega audio com numero do div
+    const audio = document.querySelector(`audio[data-key="${e}"]`);
+
+    //Chama função sonora
+    TocaSom(key, audio);
+}
+
+TocaSom = (key, audio) => {
+
+  // se audio não existir encerra iteração
+  if (!audio) return;
+
+  //Adiciona classe playing a div
+  key.classList.add('playing');
+
+  //Volta o play para zero.. caso já tenha tocado pode tocar de novo
+  audio.currentTime = 0;
+
+  //Toca audio
+  audio.play();
+
+}
+
+
+
 8878878878887889889889
   //Função que remove classe playing
   limpaClasse = (e) => {
@@ -33,4 +54,7 @@ playSound = (e) => {
   keys.forEach(key => key.addEventListener('transitionend', limpaClasse));
 
   //EventListener que dispara playSound se alguma tecla for apertada
-  window.addEventListener('keydown', playSound);
+  window.addEventListener('keydown', tocaTecla);
+
+    //EventListener que dispara playSound se alguma tecla for apertada
+
