02149c2b8ddee36a4fc5f6bacafd9c4002309b47
TTamosauskas <thiago.gt@gmail.com>
2020-03-02 11:54:33 -0300

    feat:listagem de repositorios usando api do git e recrusos do es6

--- app-ie6/src/functions.js
+++
@@ -1,7 +0,0 @@
-export const idade = 23;
-
-export default class Usuario {
- static info() {
- console.log('Apenas teste');
- }
-}
--- app-ie6/src/main.js
+++ app-ie6/src/main.js
@@ -1,7 +1,12 @@
+import api from './api';
+
+//ES6+ Criação de Classe
 class App {
   constructor (){
     this.repositories = [];
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
+    this.inputEl = document.querySelector('input[name=repository]');
     this.registerHandlers();
   }
 
@@ -10,18 +15,84 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);    
   }
 
-  addRepository(event){
+  //ES6+ Valor Padrão loading true
+  setLoading(loading = true){
+    if(loading === true){
+      let loadingEl = document.createElement('div');
+      loadingEl.appendChild(document.createTextNode("Carregando"));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl)
+    } else {
+      document.getElementById('loading').remove();
+    }
+
+  }
+
+  //ES6+ Async e Await
+  async addRepository(event){
     event.preventDefault();
 
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.lenght === 0) {return;}
+
+    this.setLoading();
+   
+    try{
+    //ES6+ Template Literals
+    const response = await api.get(`/repos/${repoInput}`);
+
+    //ES6+ Desestruturação
+    const {name, description, html_url, owner: {avatar_url}} = response.data;
+
     this.repositories.push({
-      name: 'Nome do Repositorio Dinamico',
-      description: 'Descrição Dinamica',
-      avatar_url:'https://avatars0.githubusercontent.com/u/28296274?v=4',
-      html_url: 'https://github.com/TTamosauskas/javascript-para-minha-avo/tree/master/semana-5', 
+      //ES6+ Object Short Syntax
+      name,
+      description,
+      avatar_url,
+      html_url,
     });
 
-    console.log(this.repositories)
+    this.inputEl.value = '';
+
+    this.render();
+  } catch(err){
+    alert("Repositório Inexistente")
   }
+
+  this.setLoading(false);
+  }
+
+  render(){
+  this.listEl.innerHTML= "";
+  //ES6+  Map Array
+  this.repositories.map(repo => {
+    let imgEl = document.createElement("img");
+    imgEl.setAttribute('src', repo.avatar_url);
+
+    let titleEl = document.createElement("strong");
+    titleEl.appendChild(document.createTextNode(repo.name));
+
+    let descriptionEl = document.createElement("p");
+    descriptionEl.appendChild(document.createTextNode(repo.description))
+
+    let linkEl = document.createElement("a");
+    linkEl.setAttribute('target', '_blank');
+    linkEl.setAttribute('href', repo.html_url);
+    linkEl.appendChild(document.createTextNode("Acessar"));
+
+    let listItemEl = document.createElement("li");
+    listItemEl.appendChild(imgEl);
+    listItemEl.appendChild(titleEl);
+    listItemEl.appendChild(descriptionEl);
+    listItemEl.appendChild(linkEl);
+
+    this.listEl.appendChild(listItemEl)
+  }
+  );
+}
+
 }
 
 new App();
